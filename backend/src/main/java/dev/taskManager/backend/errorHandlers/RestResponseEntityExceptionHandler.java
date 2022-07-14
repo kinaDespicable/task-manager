ec2fa9ba4105994693d0508044a167274701b60c
Daniyar Kalkamanov <danik.kalkamanov@gmail.com>
2022-07-14 23:35:18 +0600

    Response entity exception handler has been added

--- backend/src/main/java/dev/taskManager/backend/errorHandlers/RestResponseEntityExceptionHandler.java
+++ backend/src/main/java/dev/taskManager/backend/errorHandlers/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,11 @@
 package dev.taskManager.backend.errorHandlers;
 
+import dev.taskManager.backend.errorHandlers.exceptions.RoleAlreadyExistException;
+import dev.taskManager.backend.errorHandlers.exceptions.RoleNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -10,4 +15,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<ErrorMessage> roleDoesNotExistException(RoleNotFoundException exception){
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+    @ExceptionHandler(RoleAlreadyExistException.class)
+    public ResponseEntity<ErrorMessage> roleAlreadyExistException(RoleAlreadyExistException exception){
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.CONFLICT,
+                HttpStatus.CONFLICT.value(),
+                exception.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(message);
+    }
 }
